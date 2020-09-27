4905ee5a501c121d9c6215ca2a2fb66c60153f33
ulises <ulisescamps9@gmail.com>
2020-09-27 12:19:44 -0500

    toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoListItem } from "./TodoListItem";
 
-const todos = [
+const initialTodos: Array<Todo> = [
   { text: "Walk the dog", complete: true },
   { text: "Write app", complete: false },
 ];
 const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos)
+
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if (todo == selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        }
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
   return (
     <React.Fragment>
-      <TodoListItem todo={todos[0]} />
-      <TodoListItem todo={todos[1]} />
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
     </React.Fragment>
   );
 };
--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,15 +1,23 @@
 import React from "react";
-import './TodoListItem'
+import "./TodoListItem";
 
 interface TodoListItemProps {
   todo: Todo;
+  toggleTodo: ToggleTodo;
 }
 
-export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+export const TodoListItem: React.FC<TodoListItemProps> = ({
+  todo,
+  toggleTodo,
+}) => {
   return (
     <li>
       <label className={todo.complete ? "complete" : undefined}>
-        <input type="checkbox" checked={todo.complete} />
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={() => toggleTodo(todo)}
+        />
         {todo.text}
       </label>
     </li>
