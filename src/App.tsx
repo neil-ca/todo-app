d496b53424e146ee52c39382ce1715f4a6cf2847
ulises <ulisescamps9@gmail.com>
2020-09-26 20:02:54 -0500

    harcoding the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
-import React, { Component, ReactNode } from "react";
-import "./App.css";
-import axios from 'axios'
-import {Todo} from './components/Todo'
+import React from "react";
+import { TodoListItem } from "./TodoListItem";
 
-class App extends Component {
-  state = {
-    title: "test",
-    todos: []
-  };
-  componentDidMount(): void {
-    axios.get('http://localhost:8080/todos')
-    .then((res : any) => {
-      const todos = res.data
-      this.setState({ todos })
-      console.log(todos);
-      
-    })
-  }
-  render(): ReactNode {
-    return (
-      <div className="App">
-        <h1>{this.state.title}</h1>
-        <Todo todos={ this.state.todos } onSubmit = { () => console.log()}/>
-      </div>
-    );
-  }
-}
+const todos = [
+  { text: "Walk the dog", complete: true },
+  { text: "Write app", complete: false },
+];
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <TodoListItem todo={todos[0]} />
+      <TodoListItem todo={todos[1]} />
+    </React.Fragment>
+  );
+};
 
 export default App;
---
+++ src/TodoListItem.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './TodoListItem'
+
+interface TodoListItemProps {
+  todo: Todo;
+}
+
+export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+  return (
+    <li>
+      <label className={todo.complete ? "complete" : undefined}>
+        <input type="checkbox" checked={todo.complete} />
+        {todo.text}
+      </label>
+    </li>
+  );
+};
