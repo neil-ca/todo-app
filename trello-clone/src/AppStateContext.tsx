0bebad910851f177e8fce0b2b5d1236a55978697
ulises <ulisescamps9@gmail.com>
2020-12-23 02:47:17 -0600

    implement our context for now the data is hardcore it

--- trello-clone/src/AppStateContext.tsx
+++ trello-clone/src/AppStateContext.tsx
@@ -1,33 +1,47 @@
-import React,{ createContext } from "react";
+import React, { createContext } from "react";
+
 interface Task {
-    id: string
-    text: string
+  id: string;
+  text: string;
 }
 interface List {
-    id: string
-    text: string
-    tasks: Task[]
+  id: string;
+  text: string;
+  tasks: Task[];
 }
-
 export interface AppState {
-    lists: List[]
+  lists: List[];
 }
-const appData: AppState = [
-    lists: [
-        {
-            id: "0",
-            text: "To Do",
-            tasks: [{ id: "c0", text: "Generate app"}]
-        },
-        {
-            id: "1",
-            text: "In Progess",
-            tasks: [{ id: "c2", text: "Learn typescript"}]
-        },
-        {
-            id: "2",
-            text: "Done",
-            tasks: [{ id: "c3", text: "build a site"}]
-        },
-    ]
-]
+interface AppStateContextProps {
+  state: AppState;
+}
+const AppStateContext = createContext<AppStateContextProps>(
+  {} as AppStateContextProps
+);
+
+const appData: AppState = {
+  lists: [
+    {
+      id: "0",
+      text: "To Do",
+      tasks: [{ id: "c0", text: "Generate app" }],
+    },
+    {
+      id: "1",
+      text: "In Progess",
+      tasks: [{ id: "c2", text: "Learn typescript" }],
+    },
+    {
+      id: "2",
+      text: "Done",
+      tasks: [{ id: "c3", text: "build a site" }],
+    },
+  ],
+};
+export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
+  return (
+    <AppStateContext.Provider value={{ state: appData }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
