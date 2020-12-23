1ac85b1a3689084dba4edeedd6551e4b5922783d
ulises <ulisescamps9@gmail.com>
2020-12-22 19:50:42 -0600

    using reducer

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { AppContainer } from "./styles";
 import { Card } from "./components/Card";
 import { Column } from "./components/Column";
 import { AddNewItem } from "./components/AddNewItem";
 
+interface State {
+  count: number;
+}
+type Action =
+  | {
+      type: "increment";
+    }
+  | {
+      type: "decrement";
+    };
+const counterReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 function App() {
+  const [state, dispatch] = useReducer(counterReducer, { count: 0})
   return (
+    <>
+    <p>Count: {state.count}</p>
+    <button onClick={() => dispatch({ type: "decrement"})}>-</button>
+    <button onClick={() => dispatch({ type: "increment"})}>+</button>
     <AppContainer>
       <Column text="To Do">
         <Card text="Generate backend" />
@@ -16,8 +41,9 @@ function App() {
       <Column text="Done">
         <Card text="Component defined" />
       </Column>
-      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log}/>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
+    </>
   );
 }
 
